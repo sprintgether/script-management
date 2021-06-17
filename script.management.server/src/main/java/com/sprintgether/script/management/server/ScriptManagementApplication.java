96b9ce84d3c5f762c75d1e1404b55d299dca064d
simoganger <simonsoft7@gmail.com>
2021-06-17 16:31:37 +0100

    Initialisation des projets client et serveur

---
+++ script.management.server/src/main/java/com/sprintgether/script/management/server/ScriptManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sprintgether.script.management.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptManagementApplication.class, args);
+	}
+
+}
---
+++ script.management.server/src/test/java/com/sprintgether/script/management/server/ScriptManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintgether.script.management.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
