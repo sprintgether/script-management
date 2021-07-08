125c0262e248a147214b68e50793eb42e72690b8
wanefkdd@gamil.com <wanefkdd@gamil.com>
2021-07-08 19:25:03 +0100

    simulation des enregistrements dans l'éditeur quill-editor

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,26 @@ export class AppComponent implements OnInit {
   value: any = 'lewayne lewayne';
   title = 'client';
   ngOnInit(){
-      this.value = localStorage.getItem('monChat');;
+      this.value = localStorage.getItem('currentValueEditor');;
   }
 
   onChange(event: string) {
     this.value = event;
     //this.propagateChange(event); // <- you export the value so the binded variable is updated
-    localStorage.setItem('monChat', this.value);
+    
     //localStorage.setItem('monChat1', "-----------------this.value------------");
+ }
+
+ saved(){
+   if(this.value){
+    localStorage.setItem('currentValueEditor', this.value);
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Your work has been saved',
+      showConfirmButton: false,
+      timer: 1500
+    })
+   }
  }
 }
